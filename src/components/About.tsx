cd4cfe7bdb8e6416239afdc2d5315a390e38a4b7
Andres Fernandez <46586316+andyf9903@users.noreply.github.com>
2024-07-18 20:35:32 -0400

    experience component complete

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,13 +11,13 @@ const About = forwardRef<HTMLDivElement>((props, ref) => {
         <div ref={ref} className=" bg-slate-200 h-screen flex items-center mt-4 justify-center text-6xl">
             <div className="container flex w-10/12 h-3/4 border" >
                 <div className="w-3/4 flex flex-col pl-6 pr-6 border mt-6 overflow-auto break-words"  >
-                    <h1 className="font-extrabold underline underline-offset-8" >About Me.</h1>
+                    <h1 className="font-extrabold underline underline-offset-8 text-blue-900" >About Me.</h1>
                     <p className="text-[20px] mt-4 text-black tracking-wide leading-10" >
                         I am a student at Florida International University pursuing a Bachelor of Science in
-                        <span className="font-extrabold text-blue-700"> Electrical Engineering </span> with a Minor <br /> in <span className="font-extrabold text-blue-700">Computer Science</span>. I have a passion for both <span className="font-extrabold text-blue-700"> software </span>
-                        and <span className="font-extrabold text-blue-700"> hardware</span>. I am always striving to learn
-                        new things and my interests are broad. My technical skills include <span className="text-blue-700" >Javascript, C, Vue.js, React, MongoDB, HTML/CSS, Python, </span> and <span className="text-blue-700" >Java</span>. The most important thing to me professionally is
-                        working on something that <span className="font-extrabold text-blue-700"> impacts people's lives</span>.
+                        <span className="font-extrabold text-blue-900"> Electrical Engineering </span> with a Minor <br /> in <span className="font-extrabold text-blue-900">Computer Science</span>. I have a passion for both <span className="font-extrabold text-blue-900"> software </span>
+                        and <span className="font-extrabold text-blue-900"> hardware</span>. I am always striving to learn
+                        new things and my interests are broad. With a strong foundation in <span className="font-extrabold text-blue-900" >web development</span>, I've had the opportunity to work as an intern at Miami-Dade County, where I modernized parts of a website using <span className="font-extrabold text-blue-900" >Javascript</span> and <span className="font-extrabold text-blue-900" >Vue.js</span>. Currently, I am a student research anaylst at the Artificial Intelligence Laboratory under the guidance of Professor Sumit Jha, diving deeper into Artificial Intelligence and Machine learning. The most important thing to me professionally is
+                        working on something that <span className="font-extrabold text-blue-900"> impacts people's lives</span>.
                     </p>
                 </div>
                 <div className="flex flex-col border h-full justify-center w-1/2" >
@@ -26,7 +26,7 @@ const About = forwardRef<HTMLDivElement>((props, ref) => {
                             <img src={Plus} alt="plus" width="120px" />
                             <img className="ml-4" src={Laptop} alt="laptop" width="180px" />
                     </div>
-                    <div className="flex border justify-center ml-5 text-blue-700 gap-36 items-center h-56" >
+                    <div className="flex border justify-center ml-5 text-blue-900 gap-36 items-center h-56" >
                         <a className="hover:cursor-pointer" target="_blank" href="https://github.com/andresdanfernandez" >
                             <img src={Github_Logo} alt="Github Logo" width="70px" />
                             <p className="text-[14px] mt-2 ml-[0.9rem]" >Github</p>
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,17 +1,48 @@
 import { forwardRef } from "react";
+import AI from "../assets/ai.svg";
+import Brain from "../assets/brain.svg";
 
 const Experience =  forwardRef<HTMLDivElement>((props, ref) => {
 
     return (
         <div ref={ref} className="h-screen bg-slate-200 mt-4 flex items-center justify-center text-4xl border">
-            <div className="border border-red-700 justify-center w-10/12 h-5/6" >
-                <div className="h-20  border flex justify-center border-blue-800" >
-                    <h1 className="font-extrabold underline underline-offset-[14px] text-6xl" >Experience.</h1>
+            <div className="justify-center w-10/12 h-5/6" >
+                <div className="h-20  border flex justify-center" >
+                    <h1 className="font-extrabold underline underline-offset-[14px] text-6xl text-blue-900" >Experience.</h1>
                 </div>
-                <div className="border border-blue-800 flex flex-col w-full h-5/6" >
-                    <div className="border-green-700 mx-5 mr-5 ml-5 w-60 h-96" >
+                <div className="flex flex-row justify-between w-full h-5/6" >
 
+                    <div className="mx-5 mt-5 w-full h-full" >
+                        <h1 className="text-[22px]" >Information Tech Intern | Miami-Dade County</h1>
+                        <ul className="text-[18px] list-disc pl-5 ml-6 space-y-3 mt-4" >
+                            <li>
+                                <p>Developed parts of the front-end of a company website using <span className="font-extrabold text-blue-900" >Vue.js</span> and <span className="font-extrabold text-blue-900" >Javascript</span></p>
+                            </li>
+                            <li>
+                                <p>Replaced <span className="font-extrabold text-blue-900">Java</span> server-side code that used JSP to <span className="font-extrabold text-blue-900">Javascript</span> for a more component based system</p>
+                            </li>
+                            <li>
+                                <p>Collaborated with the development team to ensure a smooth transition to the new front-end architecture</p>
+                            </li>
+                            <li>
+                                <p>Improved code maintainability and readability saving future developer hours</p>
+                            </li>
+                        </ul>
                     </div>
+                    <div className=" h-full mt-6 border border-l-[5px] border-black"></div>
+                    <div className="mx-5 mt-5 w-full h-full">
+                        <h1 className="text-[22px]" >Research Assistant | Florida International University</h1>
+                        <ul className=" text-[18px] list-disc pl-5 ml-6 space-y-3 mt-4" >
+                            <li>
+                                <p>Recently started working with Dr. Sumit Jha in his <span className="font-extrabold text-blue-900" >Artificial Intelligence</span> Laboratory at FIU</p>
+                            </li>
+                        </ul>
+                        <div className="border flex gap-20 justify-center mt-16" >
+                            <img src={AI} alt="AI Icon" width="120px" />
+                            <img src={Brain} alt="Brain Icon" width="105px" />
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
